62ed01d4367d8a3e362bdec9525e3b1a0686b066
yeonjujoeng0511 <yeonjujeong0511@gmail.com>
2022-09-07 09:31:43 +0900

    setTimeout 함수 연습

--- test.js
+++ test.js
@@ -1,4 +1,20 @@
-console.log("a");
-console.log("b");
-console.log("c");
-
+setTimeout(function(){
+  console.log("a");
+  setTimeout(function(){
+      console.log("b");
+      setTimeout(function(){
+          console.log("c");
+          setTimeout(function(){
+            console.log("d")
+              setTimeout(function(){
+                console.log("e")
+                  setTimeout(function(){
+                    console.log("f")
+                  },2000)
+              },2000)
+          },1000)
+        },2000)
+    },1000)
+},3000);  // 1초 --> 1000
+  
+// 다시 부른다 --> 콜백
